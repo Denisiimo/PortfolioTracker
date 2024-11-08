89b34855e8b3b9853ef1a890d02a149be1675568
Denisiimo <denischorny22@gmail.com>
2024-11-08 00:02:35 +0000

    Completer working Form toggling, added accurate total balance display.

---
+++ PortfolioTracker/Validation/MinDoubleValueAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class MinDoubleValueAttribute : ValidationAttribute
+{
+    private readonly double _minValue;
+
+    public MinDoubleValueAttribute(double minValue)
+    {
+        _minValue = minValue;
+        ErrorMessage = $"Value must be greater than {_minValue}.";
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        // Ensure the value is a double and greater than the minimum value
+        if (value is double doubleValue)
+        {
+            if (doubleValue <= 0)
+            {
+                return new ValidationResult("Value must be greater than 0.");
+            }
+
+            if (doubleValue < _minValue)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+        else if (value is decimal decimalValue)
+        {
+            if (decimalValue <= 0)
+            {
+                return new ValidationResult("Value must be greater than 0.");
+            }
+
+            if (decimalValue < (decimal)_minValue)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
